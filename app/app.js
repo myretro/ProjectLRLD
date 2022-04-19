e91e7c2318bf8b538bf1627f44acaa0755a0d5a7
Abhinav Kumar <abhinav.vikash2@gmail.com>
2022-04-19 15:37:32 +0530

    Added routing templates to app

--- app/app.js
+++ app/app.js
@@ -4,12 +4,33 @@ const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+//adding middlewares
 // fixed : add ejs as template engine
 app.set("view engine", "ejs");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//GET Routes
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.get("/signin", (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("register");
+});
+
+//POST Routes
+app.post("/", (req, res, next) => {});
+
+app.post("/signin", (req, res, next) => {});
+
+app.post("/signup", (req, res, next) => {});
 
 app.listen(port, () => {
   console.log(`Server is on http://localhost:${port}`);

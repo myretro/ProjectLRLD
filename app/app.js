09449cd77cbadf6b60f260708c2824e0ce6a9a55
Vikash Kumar <director.iiests1@gmail.com>
2022-04-19 19:58:07 +0530

    Fixed css not working issue

--- app/app.js
+++ app/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 // fixed : add ejs as template engine
 app.set("view engine", "ejs");
+// fixed : working css in nodejs
+app.use(express.static(path.normalize(__dirname + "/../views")));
 
 app.get("/", (req, res) => {
   res.render("index");

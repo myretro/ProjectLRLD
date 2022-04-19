e8b3e582b75e30295b611e43cc64228480baea89
Abhinav Kumar <abhinav.vikash2@gmail.com>
2022-04-19 12:58:04 +0530

    Fixed add ejs as template engine

---
+++ app/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3000;
+// fixed : add ejs as template engine
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`Server is on http://localhost:${port}`);
+});

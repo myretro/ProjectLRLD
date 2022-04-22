99aad92197f9426529d983c8b53196e5c27e8371
Abhinav Kumar <abhinav.vikash2@gmail.com>
2022-04-22 16:24:20 +0530

    Fixed : Signin route v1.0

--- app/app.js
+++ app/app.js
@@ -1,11 +1,15 @@
 require("../configs/db.config");
+const initialize = require("../middlewares/passport.middleware");
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const User = require("../models/user.model");
+const passport = require("passport");
+const session = require("express-session");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const secret = process.env.SECRET || "abhinav";
 
 //adding middlewares
 // fixed : add ejs as template engine
@@ -14,10 +18,36 @@ app.set("view engine", "ejs");
 app.use(express.static(path.normalize(__dirname + "/../views")));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.session());
+app.use(passport.initialize());
+initialize(passport);
+
+//check if user is already auth
+const checkAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.redirect("/fail");
+};
+
+//check if user is not auth
+const checkNotAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return res.redirect("/fail");
+  }
+  return next();
+};
 
 //GET Routes
-app.get("/", (req, res) => {
+app.get("/", checkNotAuth, (req, res) => {
   res.render("index");
 });
 
@@ -30,11 +60,28 @@ app.get("/signup", (req, res) => {
 });
 
 //POST Routes
-app.post("/", (req, res, next) => {});
+app.post("/", checkAuth, (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/", {
+    name: req.user.name,
+  });
+});
 
-app.post("/signin", (req, res, next) => {});
+app.post(
+  "/signin",
+  passport.authenticate("local", {
+    failureRedirect: "/fail",
+  }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
 
-app.post("/signup", async (req, res) => {
+app.post("/fail", (req, res) => {
+  res.status(500).json({ message: "Invalid Credentials" });
+});
+
+app.post("/signup", checkNotAuth, async (req, res) => {
   const { name, email, password, welcome_message } = req.body;
   if (!(name && email && password)) {
     return res.status(400).json({ message: "All fields are required" });

c5d7fdb95e3a1eeb54f9690ddb9514c865cb47e3
Abhinav Kumar <abhinav.vikash2@gmail.com>
2022-04-22 10:56:58 +0530

    Added Signup Route

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ require("../configs/db.config");
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const User = require("../models/user.model");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -33,7 +34,28 @@ app.post("/", (req, res, next) => {});
 
 app.post("/signin", (req, res, next) => {});
 
-app.post("/signup", (req, res, next) => {});
+app.post("/signup", async (req, res, next) => {
+  const { name, email, password, welcome_message } = req.body;
+  if (!(name && email && password)) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  //check for old users
+  const oldUser = await User.findOne({ email });
+  if (oldUser) {
+    res.status(400).json({ message: "User already exists." });
+  } else {
+    const user = new User({
+      name,
+      email,
+      password,
+      welcome_message,
+    });
+
+    await user.save();
+    res.status(200).json({ data: user.email });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is on http://localhost:${port}`);
